916542b707872f688676e2b4267a97a6aa7389df
testforautism <49126347+testforautism@users.noreply.github.com>
2019-04-01 00:49:27 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -106,7 +106,7 @@
   }
 }( jQuery ));
 
-$('#ActivityDIV'   ).makeSVGcolor('/images/colorit.svg')
+$('#ActivityDIV'   ).makeSVGcolor('colorit.svg')
 $('#btnRandom'     ).btnRandom()
 $('#btnClear'      ).btnClear()
 $('#btnDownloadSVG').btnDownload()
